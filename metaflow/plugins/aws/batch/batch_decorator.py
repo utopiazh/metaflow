0ee069c6f194d642d91cda9aa7832603b9a71d85
Savin <savingoyal@gmail.com>
2021-06-28 09:29:50 -0700

    Refactor @resources decorator

--- metaflow/plugins/aws/batch/batch_decorator.py
+++ metaflow/plugins/aws/batch/batch_decorator.py
@@ -10,6 +10,7 @@ from metaflow.datastore.datastore import TransformableObject
 from metaflow.datastore.util.s3util import get_s3_client
 from metaflow.decorators import StepDecorator
 from metaflow.metaflow_config import DATASTORE_LOCAL_DIR
+from metaflow.plugins import ResourcesDecorator
 from metaflow.plugins.timeout_decorator import get_run_time_limit_for_task
 from metaflow.metadata import MetaDatum
 
@@ -29,39 +30,6 @@ except:  # noqa E722
     # python3
     from urllib.parse import urlparse
 
-
-class ResourcesDecorator(StepDecorator):
-    """
-    Step decorator to specify the resources needed when executing this step.
-    This decorator passes this information along to Batch when requesting resources
-    to execute this step.
-    This decorator is ignored if the execution of the step does not happen on Batch.
-    To use, annotate your step as follows:
-    ```
-    @resources(cpu=32)
-    @step
-    def myStep(self):
-        ...
-    ```
-    Parameters
-    ----------
-    cpu : int
-        Number of CPUs required for this step. Defaults to 1
-    gpu : int
-        Number of GPUs required for this step. Defaults to 0
-    memory : int
-        Memory size (in MB) required for this step. Defaults to 4096
-    shared_memory : int
-        The value for the size (in MiB) of the /dev/shm volume for this step.
-        This parameter maps to the --shm-size option to docker run .
-    """
-    name = 'resources'
-    defaults = {
-        'cpu': '1',
-        'gpu': '0',
-        'memory': '4096',
-        'shared_memory': None
-    }
 
 class BatchDecorator(StepDecorator):
     """
